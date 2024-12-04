c76456cd99971aac8a1c34b22094c4cef42d5f21
Egor Ivanov <rust_lang_lover@proton.me>
2024-12-04 14:59:15 +0300

    // added documentation controller

--- user-service/src/user/user.controller.ts
+++ user-service/src/user/user.controller.ts
@@ -5,6 +5,11 @@ import { UserService } from './user.service';
 export class UserController {
     constructor(private readonly userService: UserService) {}
 
+    /**
+     * GET => Замешивает случайных пользователей в бд.
+     * @param count парсит count из параметров запроса и замешивает указанное количество пользователей
+     * Если count не определен, то замешивает 1000000
+     */
     @Get('seed')
     async seedUsers(@Query('count') count: number) {
         const numUsers = parseInt(String(count), 10) || 1000000;
@@ -12,6 +17,10 @@ export class UserController {
         return { message: `Successfully added ${numUsers} users` };
     }
 
+    /**
+     * GET => Проверяет у кого из пользователей установлен hasProblems в значении true и возвращает в false
+     * Возвращает количество пользователей, у которых значение было обновлено.
+     */
     @Get('reset-problems')
     async resetProblems() {
         const count = await this.userService.resetProblems();
