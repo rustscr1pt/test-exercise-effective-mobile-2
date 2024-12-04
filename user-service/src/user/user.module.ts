2987f26b3ef6b482dd35b2c5d0538680c2f4d9a8
Egor Ivanov <rust_lang_lover@proton.me>
2024-12-04 15:11:54 +0300

    // docs

--- user-service/src/user/user.module.ts
+++ user-service/src/user/user.module.ts
@@ -5,9 +5,9 @@ import { UserController } from './user.controller';
 import {User} from "./entities/user/user";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])], // Register the User entity
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [UserService],
   controllers: [UserController],
-  exports: [UserService], // Export the service if needed in other modules
+  exports: [UserService],
 })
 export class UserModule {}
